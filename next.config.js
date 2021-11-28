d571996b141052a0ca91319e65a00aed2f9086eb
harajune <harajune@gijutsuya.jp>
2021-11-29 00:15:39 +0900

    tweaks

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-const { https } = require('firebase-functions')
-const { default: next } = require('next')
-
-const nextjsDistDir = require('./next.config.js').distDir
-
-const nextjsServer = next({
-  dev: false,
-  conf: {
-    distDir: nextjsDistDir,
-  },
-})
-const nextjsHandle = nextjsServer.getRequestHandler()
-
-exports.nextjsFunc = https.onRequest((req, res) => {
-  return nextjsServer.prepare().then(() => nextjsHandle(req, res))
-})
+module.exports = {
+  distDir: './.next',
+}
