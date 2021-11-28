12508836f56e749d3438bb6706eac418a722fe6e
harajune <harajune@gijutsuya.jp>
2021-11-28 16:49:10 +0900

    change defailt page to test page

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '6dd5f63d8b464068855d817adf6610ee',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
